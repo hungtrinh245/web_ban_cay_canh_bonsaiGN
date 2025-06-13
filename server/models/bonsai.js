7980bb43a7e1c79cfa669435090728f97d563986
hungtrinh245 <trinhquochung222005@gmail.com>
2025-06-13 22:40:44 +0700

    start server

--- server/models/bonsai.js
+++ server/models/bonsai.js
@@ -1,3 +1,30 @@
 const bonsaiSchema = new mongoose.Schema({
-    
-})
+    name: {
+        type: String,
+        require: [true, 'Tên sản phẩm không được để trống'],
+        trim: true
+    },
+      devription: {
+        type: String,
+        require: [true, 'Mô tả sản phẩm không được để trống'],  
+    },
+      price: {
+        type: Number,
+        require: [true, 'Giá sản phẩm không được để trống'],  
+    },
+     images: [{ 
+        type: String, 
+        required: false 
+    }],
+    category: {
+        type: String,
+        required: [true, 'Danh mục sản phẩm không được để trống'],
+        // enum: ['phong thủy', 'để bàn', Cây để bàn', 'Cây thủy sinh', 'Xương rồng', 'Sen đá', 'Cây dây leo'] 
+    },
+     createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+const Bonsai = mongoose.model('Bonsai', bonsaiSchema);
+module.exports = Product;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-// E:\web ban cay canh_bonsaiGN\cay_canh\server\server.js
 
-require('dotenv').config(); // Nạp biến môi trường từ .env
+
+require('dotenv').config(); 
 const express = require('express');
 const cors = require('cors');
 
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 5001; // Sử dụng port khác với React
 // Middleware
 app.use(cors()); // Cho phép tất cả các origin truy cập
 app.use(express.json()); // Parse request body dạng JSON
-app.use(express.urlencoded({ extended: true })); // Parse request body dạng x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); 
 
 // Route API cơ bản để kiểm tra
 app.get('/api/test-server', (req, res) => {
   res.json({ message: 'Backend server đang hoạt động tốt!' });
-});
+}); 
 
-// Khởi động server sau đó chạy serverserver
+// Khởi động server sau đó chạy server
 app.listen(PORT, () => {
   console.log(`Backend server mới đang chạy trên http://localhost:${PORT}`);
 });
